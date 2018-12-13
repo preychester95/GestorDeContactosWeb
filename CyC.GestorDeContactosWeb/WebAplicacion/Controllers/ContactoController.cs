c43d6bf7b4294a19f504f89657eb2aa3af6885a2
Pablo Rey Campo <PRC@cyc.es>
2018-12-13 08:38:56 +0100

    +primera version de la web cargando datos en tabla

---
+++ CyC.GestorDeContactosWeb/WebAplicacion/Controllers/ContactoController.cs
@@ -0,0 +1,34 @@
+﻿using CyC.GestorDeContactos.AccesoDatos.DAL;
+using CyC.GestorDeContactos.AccesoDatos.DTO;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace WebAplicacion.Controllers
+{
+    public class ContactoController : Controller
+    {
+        // GET: Contacto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            List<Contacto> listaContactos = ContactoDAL.getAllContactos();
+            return Json(new { data = listaContactos }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int i = 0)
+        {
+            return View(new Contacto());
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit()
+        {
+            return View();
+        }
+    }
+}
